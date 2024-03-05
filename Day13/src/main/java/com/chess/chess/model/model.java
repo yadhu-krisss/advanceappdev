dfb173f5ecb1a3332d18332c492908ae3aed9241
yadhu-krisss <119097296+yadhu-krisss@users.noreply.github.com>
2024-03-05 16:21:09 +0530

    Add files via upload

---
+++ Day13/src/main/java/com/chess/chess/model/model.java
@@ -0,0 +1,27 @@
+package com.chess.chess.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "UserRegister")
+
+public class model {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String confirmpassword;
+}
